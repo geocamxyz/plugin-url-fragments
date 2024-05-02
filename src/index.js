e62892c89434af71bcdbed9e40e6cd748da7d4d2
Julian Cox <julian.cox@webspeed.co.nz>
2024-05-03 10:52:08 +1200

    added url-fragments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-export { urlFragments } from './lib/url-fragments.js';
+import { urlFragments } from "./lib/url-fragments.js";
+
+export class GeocamViewerUrlFragments extends HTMLElement {
+  constructor() {
+    super();
+    this.plugin = null;
+    // this.yaw = this.getAttribute('yaw') || 0;
+    console.log("url-fragments init");
+  }
+
+  connectedCallback() {
+    console.log("url-fragments connected");
+    const node = this;
+    const parent = this.parentNode;
+    if (parent.viewer && parent.viewer.plugin) {
+      // Call a method on the parent
+      //  new urlFragments({stores: ['fov','facing','horizon','shot','sli','visible','left','top','width','height','mode','autorotate','autobrightness','zoom','center']}),
+      const params = this.getAttribute("params") || "";
+      const stores = params.split(",");
+      this.plugin = new urlFragments({ stores: stores });
+      parent.viewer.plugin(this.plugin);
+    } else {
+      console.error(
+        "GeocamViewerUrlFragments must be a child of GeocamViewer"
+      );
+    }
+  }
+
+  disconnectedCallback() {
+    this.plugin = null;
+    console.log("url-fragments disconnected");
+    // Clean up the viewer
+  }
+}
+
+window.customElements.define(
+  "geocam-viewer-url-fragments",
+  GeocamViewerUrlFragments
+);
