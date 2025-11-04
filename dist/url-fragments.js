659f5e8601d83c4762114b989456f2d897da32b1
Luke Reid <luke@reid.org.nz>
2025-11-04 16:04:50 +1300

    Update to version 2.2.5rc7 in plugins/url-fragments

--- dist/url-fragments.js
+++ dist/url-fragments.js
@@ -1,73 +1,83 @@
-const S = function(r = {}) {
-  let s = new URLSearchParams(window.location.hash.substr(1));
-  const a = r.stores || "*";
-  let n, c, l = "", u, d = {};
-  const f = 100, p = function() {
-    let e = s.toString();
-    e !== l && (l = e, /* console.log("pushing state", e) */ history.pushState(null, "", `#${e}`));
+const S = function(l = {}) {
+  let n = new URLSearchParams(window.location.hash.substr(1));
+  const o = l.stores || "*";
+  let s, c, r = "", u, d = {};
+  const m = 100, p = function() {
+    let e = n.toString();
+    e !== r && (r = e, history.pushState(null, "", `#${e}`));
   }, v = function() {
-    clearTimeout(c), c = setTimeout(p, f);
+    clearTimeout(c), c = setTimeout(p, m);
   }, h = function() {
-    for (let [e, t] of s.entries())
-      if (a === "*" || a.includes(e)) {
-        let o;
+    for (let [e, t] of n.entries())
+      if (o === "*" || o.includes(e)) {
+        let i;
         try {
-          o = JSON.parse(t);
-        } catch (i) {
-          if (i instanceof SyntaxError)
-            o = t, console.warn(
+          i = JSON.parse(t);
+        } catch (a) {
+          if (a instanceof SyntaxError)
+            i = t, console.warn(
               `urlFragments: could not parse JSON value for ${e} in URL hash: ${t}`
             );
           else
-            throw i;
+            throw a;
         }
-        n.stores[e] ? n.stores[e](o) : n.store(e, o);
+        s.stores[e] ? s.stores[e](i) : s.store(e, i);
       }
   }, b = function(e) {
     for (let t in e)
-      if ((a === "*" || a.includes(t)) && !d[t]) {
-        const o = e[t];
-        d[t] = o((i) => {
-          const g = JSON.stringify(i);
-          s.get(t) !== g && (s.set(t, g), v());
+      if ((o === "*" || o.includes(t)) && !d[t]) {
+        const i = e[t];
+        d[t] = i((a) => {
+          const g = JSON.stringify(a);
+          n.get(t) !== g && (n.set(t, g), v());
         });
       }
   }, w = function(e) {
-    s = new URLSearchParams(window.location.hash.substr(1)), h();
-  }, m = function(e) {
+    n = new URLSearchParams(window.location.hash.substr(1)), h();
+  }, f = function(e) {
     console.log("windowMessage", e), e.data.message === "updateHash" && (window.location = e.data.value);
   };
   this.init = function(e) {
-    n = e, h(), u = n.newstoreadded((t) => {
-      b(n.stores);
-    }), window.addEventListener("popstate", w), window.addEventListener("message", m);
+    s = e, h(), u = s.newstoreadded((t) => {
+      b(s.stores);
+    }), window.addEventListener("popstate", w), window.addEventListener("message", f);
   }, this.destroy = function() {
-    window.removeEventListener("popstate", m), window.removeEventListener("popstate", w), u();
+    window.removeEventListener("popstate", f), window.removeEventListener("popstate", w), u();
   };
 };
-class L extends HTMLElement {
+class y extends HTMLElement {
   constructor() {
-    super(), this.plugin = null, console.log("url-fragments init");
+    super(), this.plugin = null, this.viewer = null, console.log("url-fragments init");
   }
   connectedCallback() {
     console.log("url-fragments connected");
-    const s = this.parentNode;
-    if (s.viewer && s.viewer.plugin) {
-      const n = (this.getAttribute("params") || "").split(",");
-      this.plugin = new S({ stores: n }), s.viewer.plugin(this.plugin);
-    } else
+    const n = this.closest("geocam-viewer");
+    if (!n) {
       console.error(
         "GeocamViewerUrlFragments must be a child of GeocamViewer"
       );
+      return;
+    }
+    const o = () => {
+      const s = n.viewer;
+      if (s && typeof s.plugin == "function") {
+        if (this.plugin) return;
+        this.viewer = s;
+        const r = (this.getAttribute("params") || "").split(",");
+        this.plugin = new S({ stores: r }), this.viewer.plugin(this.plugin);
+      } else
+        setTimeout(o, 50);
+    };
+    o();
   }
   disconnectedCallback() {
-    this.plugin = null, console.log("url-fragments disconnected");
+    this.plugin && typeof this.plugin.destroy == "function" && this.plugin.destroy(), this.plugin = null, this.viewer = null, console.log("url-fragments disconnected");
   }
 }
 window.customElements.define(
   "geocam-viewer-url-fragments",
-  L
+  y
 );
 export {
-  L as GeocamViewerUrlFragments
+  y as GeocamViewerUrlFragments
 };
